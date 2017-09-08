4b66c296d889dce7184bf796af28ba696444d074
ZhengPeng <15732115701@163.com>
2017-09-08 13:46:50 +0800

    Delete sin_test.m

--- sin_test.m
+++
@@ -1,8 +0,0 @@
-%	用于验证程序的合理性
-clear, clc;
-x = 0 : 0.01 : 2 * pi;
-y = sin(x);
-points = [x; y]';
-result = figure_out_curvature(points);
-[x_index, y_index] = find(points(:, 1) == 0.77);
-result(x_index, :)
