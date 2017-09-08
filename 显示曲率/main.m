9a4cae7b18fb42c011c56f6be5934baf99844dbc
ZhengPeng <15732115701@163.com>
2017-09-08 13:52:35 +0800

    Update main.m

--- 显示曲率/main.m
+++ 显示曲率/main.m
@@ -1,4 +1,10 @@
-clear, clc;
+clear, clc
 close all;
-script_v2_1_task;
+
+originalImage = imread(strcat(pwd,'.\Leaves_UMU\07.jpg'));
+if numberOfColorBands > 1
+	grayImage = originalImage(:, :, 2); % Take green channel.
+else
+	grayImage = originalImage;
+end
 [k, p] = drive(binaryImage);
